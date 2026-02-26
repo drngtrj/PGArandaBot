1ce535718774cf96c9b040a333e52881ffdae2ec
drngtrj <adriangetroj@gmail.com>
2026-02-26 14:03:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,12 @@ from openai import OpenAI
 TELEGRAM_TOKEN = os.getenv("8666433188:AAFyo56YJPNngSk0phnYqv-meTjLXUCGB00")
 OPENAI_API_KEY = os.getenv("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")
 
+print("API KEY:", OPENAI_API_KEY)
+
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("API KEY:", OPENAI_API_KEY)
     texto_usuario = update.message.text
 
     respuesta = client.chat.completions.create(
@@ -23,5 +26,6 @@ async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, responder))
+
 
 app.run_polling()
